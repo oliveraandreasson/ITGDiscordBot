21c7b071df19177ee08eda23731265653497c76b
Emrik Östling <emrik.ostling@learnet.se>
2017-09-19 19:58:11 +0200

    försök till att spela musik

--- index.js
+++ index.js
@@ -4,12 +4,24 @@ const date = require("date");
 const bot = new Discord.Client();
 const prefix = "!";
 const botToken = require("./bottoken");
+const playArbitraryFFmpeg = require('discord.js-arbitrary-ffmpeg');
 
+bot.on("disconnect", () => {
+	console.log("Disconnected, trying to login again in 30 seconds");
+	// Wait 30 seconds and then try to reconnect
+	setTimeout(() => {
+		helper.login(() => helper.onReady());
+	}, 30000);
+});
+
+bot.on("warn", (m) => console.log("[warn]", m));
 
 bot.on("ready", () => {
     console.log("Klar");
     bot.user.setGame("Skriv !help för hjälp.");
 });
+
+var musicDir = "~/musik/"
 
 var borde = [
     "Ja",
@@ -62,6 +74,71 @@ bot.on("message", (message) => {
         case "vecka":
             message.channel.send(weekNumber);
             break;
+        case "join":
+            if (!message.guild) return;
+                // Only try to join the sender's voice channel if they are in one themselves
+                if (message.member.voiceChannel) {
+                message.member.voiceChannel.join()
+                    .then(connection => { // Connection is an instance of VoiceConnection
+                        message.reply("I have successfully connected to the channel!");
+                    })
+                    .catch(console.log);
+                }
+                else {
+                    message.reply("You need to join a voice channel first!");
+                }
+                let arrFFmpegParams = [
+                    '-i', 'musik/thisisnottheend.mp3',
+                    '-filter:a', 'asetrate=r=66K'
+                ];
+                
+                const objStreamDispatcher = playArbitraryFFmpeg(
+                    VoiceConnection, // A VoiceConnection from Discord.js
+                    arrFFmpegParams,
+                    {volume: .25} // Optional stream options (same as for playFile, playStream, etc.)
+                );
+            break;
+        case "play":
+            
+            break;
+        /*case "leave":
+            client.internal.leaveVoiceChannel();
+		    // Return to prevent further commands
+		    break;
+        case "stop":
+            if (client.internal.voiceConnection) {
+			// ... stop the current playback
+			    client.internal.voiceConnection.stopPlaying();
+		    }
+            break;
+        case "init":
+        // Iterate over all channels
+		    for (var channel of message.channel.server.channels) {
+			// If the channel is a voice channel, ...
+			    if (channel instanceof Discord.VoiceChannel) {
+				// ... reply with the channel name and the ID ...
+				    client.reply(message, channel.name + " - " + channel.id);
+				    // ... and join the channel
+				    client.joinVoiceChannel(channel).catch(error);
+				    // Afterwards, break the loop so the bot doesn't join any other voice
+				    // channels
+				    break;
+			    }
+            }
+            break;
+        case "play":
+            if (client.internal.voiceConnection) {
+			    // ...tell the user that you will play the file...
+			    client.reply(m, "härligt");
+			    // ...get the voice connection that is currently active...
+			    var connection = client.internal.voiceConnection;
+			    // ...get the path from which to load the file (the hardcoded directory
+			    // concatenated with the argument to the command)...
+			    var filePath = musicDir + "thisisnottheend.mp3"
+			    // ...and play the file
+			    connection.playFile(filePath);
+		    }
+            break;*/
         case "s":
         case "schema":
             var valfriVecka = message.content.substring(8)
@@ -200,5 +277,10 @@ bot.on("message", (message) => {
             message.channel.send("```Detta kommando existerar inte (än)\nSkriv !help för att se de kommandon som faktiskt existerar```");
     }
 });
+
+function error(e) {
+	console.log(e.stack);
+	process.exit(0);
+}
 
 bot.login(botToken);
