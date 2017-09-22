1db9b59cb3f8287022bf40fcf6e9f0a4daafd0f3
Emrik Östling <emrik.ostling@learnet.se>
2017-09-22 22:45:44 +0200

    La till skulle som extra för !borde

--- index.js
+++ index.js
@@ -378,6 +378,7 @@ bot.on("message", (message) => {
         case "getid":
             message.channel.send(message.author.id);
             break;
+        case "skulle":
         case "kommer":
         case "är":
         case "borde":
